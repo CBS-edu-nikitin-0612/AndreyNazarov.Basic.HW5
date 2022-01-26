7eaf343b3aed263acce23cf87d1ba0597ab0e7a0
Andrew <1@1.rugit config --global user.name Andrewcd F:\ITVDN\2 git config --global user.email 1@1.ru>
2022-01-26 23:33:13 +0300

    feat: Task6

---
+++ Task6/Dictionary.cs
@@ -0,0 +1,61 @@
+﻿namespace Task6
+{
+    internal class Dictionary
+    {
+        private string[] rus = new string[5];
+        private string[] eng = new string[5];
+        private string[] ua = new string[5];
+
+        public Dictionary()
+        {
+            rus[0] = "книга"; eng[0] = "book"; ua[0] = "Книга";
+            rus[1] = "ручка"; eng[1] = "pen"; ua[1] = "Ручка";
+            rus[2] = "солнце"; eng[2] = "sun"; ua[2] = "Сонце";
+            rus[3] = "яблоко"; eng[3] = "apple"; ua[3] = "Яблуко";
+            rus[4] = "стол"; eng[4] = "table"; ua[4] = "Стола";
+        }
+
+        public string this[string index, Language language]
+        {
+            get
+            {
+                string[] dict = null;
+                switch (language)
+                {
+                    case Language.Rus:
+                        dict = rus;
+                        break;
+                    case Language.Eng:
+                        dict = eng;
+                        break;
+                    case Language.Ua:
+                        dict = ua;
+                        break;
+                }
+                for (int i = 0; i < dict.Length; i++)
+                    if (dict[i] == index)
+                        return rus[i] + " - " + eng[i] + " - " + ua[i];
+
+                return string.Format("{0} - нет перевода для этого слова.", index);
+            }
+        }
+
+        public string this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < rus.Length)
+                    return rus[index] + " - " + eng[index] + " - " + ua[index];
+                else
+                    return "Попытка обращения за пределы массива.";
+            }
+        }
+
+        public enum Language
+        {
+            Rus,
+            Eng,
+            Ua
+        }
+    }
+}
---
+++ Task6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Task6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary dictionary = new Dictionary();
+
+            Console.WriteLine(dictionary["книга", Dictionary.Language.Rus]);
+            Console.WriteLine(dictionary["sun", Dictionary.Language.Eng]);
+            Console.WriteLine(dictionary["Стола", Dictionary.Language.Ua]);
+            Console.WriteLine(dictionary["Вино", Dictionary.Language.Rus]);
+
+
+            Console.WriteLine(new string('-', 20));
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(dictionary[i]);
+            }
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
