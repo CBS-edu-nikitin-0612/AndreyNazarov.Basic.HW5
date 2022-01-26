ac76dd8462392b5aa18cfc0ff49cbaba76fd1227
Andrew <1@1.rugit config --global user.name Andrewcd F:\ITVDN\2 git config --global user.email 1@1.ru>
2022-01-26 23:14:09 +0300

    feat:Task3

---
+++ Task3/MyMatrix.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace Task3
+{
+    internal class MyMatrix
+    {
+        private int[,] matrix = null;
+        private Random rnd;
+
+        private static MyMatrix instance = null;
+
+        public static MyMatrix GetMyMatrix()
+        {
+            if (instance == null)
+            {
+                return new MyMatrix();
+            }
+            else
+            {
+                return instance;
+            }
+        }
+        private MyMatrix()
+        {
+            matrix = new int[2, 2];
+            rnd = new Random();
+            matrix[0, 1] = getNewInt();
+            matrix[0, 0] = getNewInt();
+            matrix[1, 1] = getNewInt();
+            matrix[1, 0] = getNewInt();
+        }
+
+        private int getNewInt()
+        {
+            return rnd.Next(1, 9);
+        }
+
+        public void ChangeMatrix(int colomn, int row)
+        {
+            if (colomn <= 0 || row <= 0)
+            {
+                Console.WriteLine("Specify parameters greater than zero ");
+            }
+            else
+            {
+                var newMatrix = new int[colomn, row];
+
+                for (int c = 0; c < colomn; c++)
+                {
+                    for (int r = 0; r < row; r++)
+                    {
+                        if (matrix.GetLength(0) > c && matrix.GetLength(1) > r)
+                        {
+                            newMatrix[c, r] = matrix[c, r];
+                        }
+                        else
+                        {
+                            newMatrix[c, r] = getNewInt();
+                        }
+                    }
+                }
+                matrix = newMatrix;
+
+            }
+        }
+
+        public void PrintMatrix()
+        {
+            Console.WriteLine(this[0, matrix.GetLength(0), 0, matrix.GetLength(1)]);
+        }
+
+        public string this[int cFrom, int cTo, int rFrom, int rTo]
+        {
+            get
+            {
+                var result = "Matrix:\n";
+                for (int c = cFrom; c <= cTo - 1; c++)
+                {
+                    for (int r = rFrom; r <= rTo - 1; r++)
+                    {
+                        result += matrix[c, r];
+                    }
+                    result += "\n";
+                }
+                return result;
+            }
+        }
+    }
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var matrix = MyMatrix.GetMyMatrix();
+            matrix.PrintMatrix();
+            matrix.ChangeMatrix(10, 10);
+            matrix.PrintMatrix();
+            matrix.ChangeMatrix(5, 5);
+            matrix.PrintMatrix();
+
+            Console.WriteLine(matrix[1, 3, 1, 4]);
+        }
+    }
+}
